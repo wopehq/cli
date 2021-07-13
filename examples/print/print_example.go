18025f883a3bfc9ad28c98cbe22d39c626c9cd6f
xis_xi <fopeak@gmail.com>
2021-07-13 16:56:18 +0300

    examples: add a simple example

---
+++ examples/print/print_example.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/teamseodo/cli"
+)
+
+func main() {
+	c := cli.NewConfigurator(os.Args[1:])
+
+	mainCommand := cli.NewCommand("app", "app [command] [flags]", "description about the app")
+	c.SetMainCommand(mainCommand)
+
+	printCommand := cli.NewCommand("print", "print [flags]", "prints the values")
+	mainCommand.AddCommand(printCommand)
+
+	var messageParameter string
+	printCommand.AddStringParameter(&cli.Parameter{
+		Use:       "app print --message [value]",
+		Name:      "message",
+		Shortname: "m",
+	}, &messageParameter, "hello world")
+
+	printCommand.Do(func(cmd *cli.Command) {
+		fmt.Println(messageParameter)
+	})
+
+	help, err := c.FindHelp()
+	if err != nil {
+		log.Fatal(err)
+	}
+	help.ShowHelp()
+
+	err = c.Initialize()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
