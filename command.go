5f209191984408a1a916893cea47098080f170ca
xis_xi <fopeak@gmail.com>
2021-07-24 17:24:12 +0300

    command: add configurator's things into command

--- command.go
+++ command.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"strconv"
 
 	"github.com/fatih/color"
@@ -14,6 +15,9 @@ var green = color.New(color.FgGreen, color.Bold)
 var ErrParameterIsNotBool = errors.New("wanted parameter is not a boolean")
 var ErrParameterIsNotInt = errors.New("wanted parameter is not an integer")
 var ErrParameterIsNotString = errors.New("wanted parameter is not a string")
+var ErrCommandHandlerNotFound = errors.New("command's handler function is not found")
+var ErrCommandNotFound = errors.New("command is not found in configurator's commands")
+var ErrParameterNotFound = errors.New("wanted parameter is not found")
 
 type Command struct {
 	Name        string
@@ -41,6 +45,57 @@ func NewCommand(name string, use string, description string) *Command {
 	return command
 }
 
+// Do sets the command's handler function
+func (c *Command) Do(handler func(cmd *Command)) {
+	c.handler = handler
+}
+
+// AddCommand adds sub commands to the command
+func (c *Command) AddCommand(command *Command) *Command {
+	c.Commands = append(c.Commands, command)
+	return command
+}
+
+// Parse parses the args returns the wanted command
+func (c *Command) Parse(args []string) (*Command, error) {
+	cmd, position, err := c.getCurrentCommand(args)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(args) > 0 && cmd != c {
+		args = args[position:]
+	}
+
+	err = cmd.Flagset.Parse(args)
+	if err != nil {
+		return nil, err
+	}
+
+	return cmd, nil
+}
+
+// getCurrentCommand returns the current command for example
+// "app run concurrent -v"
+// concurrent is the current command here
+func (c *Command) getCurrentCommand(args []string) (*Command, int, error) {
+	currentCommand := c
+	var position int
+	for _, arg := range args {
+		if isFlagArg(arg) {
+			break
+		}
+
+		cmd, exists := currentCommand.FindCommand(arg)
+		if !exists {
+			return currentCommand, position, fmt.Errorf("%s %w", arg, ErrCommandNotFound)
+		}
+		currentCommand = cmd
+		position += 1
+	}
+	return currentCommand, position, nil
+}
+
 // ShowHelp prints the usage of the command
 func (c *Command) Help() *Help {
 	var help Help
@@ -63,13 +118,40 @@ func (c *Command) Help() *Help {
 	return &help
 }
 
-// Do sets the command's handler function
-func (c *Command) Do(handler func(cmd *Command)) {
-	c.handler = handler
+// Run runs the command's handler function
+func (c *Command) Run() error {
+	if c.handler == nil {
+		return fmt.Errorf("%s %w", c.Name, ErrCommandHandlerNotFound)
+	}
+
+	c.handler(c)
+	return nil
 }
 
-func (c *Command) Run() {
-	c.handler(c)
+// FindHelp recognizes if any help command is received and returns a help
+func (c *Command) FindHelp(args []string) (*Help, error) {
+	var help *Help
+	if len(args) == 0 && c.handler == nil {
+		help = c.Help()
+	}
+
+	if len(args) > 0 {
+		if args[0] == "help" {
+			if len(args) == 1 {
+				help = c.Help()
+			}
+
+			if len(args) > 1 {
+				cmd, _, err := c.getCurrentCommand(args[1:])
+				if err != nil {
+					return nil, err
+				}
+				help = cmd.Help()
+			}
+		}
+	}
+
+	return help, nil
 }
 
 // FindCommand searches the sub commands of the command
@@ -80,12 +162,6 @@ func (c *Command) FindCommand(name string) (*Command, bool) {
 		}
 	}
 	return nil, false
-}
-
-// AddCommand adds sub commands to the command
-func (c *Command) AddCommand(command *Command) *Command {
-	c.Commands = append(c.Commands, command)
-	return command
 }
 
 func (c *Command) addParameter(parameter *Parameter) {
@@ -147,4 +223,10 @@ func (c *Command) GetInt(name string) (int, error) {
 		return 0, ErrParameterNotFound
 	}
 	return strconv.Atoi(value)
+}
+
+// isFlagArg returns if the given arg is a flag or not
+func isFlagArg(arg string) bool {
+	return ((len(arg) >= 3 && arg[1] == '-') ||
+		(len(arg) >= 2 && arg[0] == '-' && arg[1] != '-'))
 }
--- command_test.go
+++ command_test.go
@@ -1,9 +1,27 @@
 package cli
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 	"testing"
 )
+
+type RunTest struct {
+	Name                string
+	Args                []string
+	Command             *Command
+	SubCommands         []*Command
+	ExpectedCommandName string
+}
+
+type GetCurrentCommandTest struct {
+	Name                   string
+	Command                *Command
+	SubCommands            []*Command
+	Args                   []string
+	ExpectedCurrentCommand string
+}
 
 type SubCommandsTest struct {
 	Name                string
@@ -17,6 +35,156 @@ type ParameterTest struct {
 	Parameter              Parameter
 	Args                   []string
 	ExpectedParameterValue interface{}
+}
+
+type FindHelpTest struct {
+	Name            string
+	Args            []string
+	Command         *Command
+	SubCommands     []*Command
+	ExpectedStrings []string
+}
+
+func TestRun(t *testing.T) {
+	tests := []RunTest{
+		{
+			Name:                "simple do command",
+			Args:                []string{"do"},
+			Command:             NewCommand("seo", "seo [command]", ""),
+			ExpectedCommandName: "do",
+			SubCommands: []*Command{
+				{
+					Name: "do",
+					Use:  "seo do [flags]",
+					Parameters: []Parameter{
+						{
+							Name:      "message",
+							Shortname: "m",
+						},
+					},
+					Flagset: flag.NewFlagSet("cli", flag.ContinueOnError),
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			var commandName string
+			for _, cmd := range test.SubCommands {
+				cmd.Do(func(cmd *Command) {
+					if cmd.Name == test.ExpectedCommandName {
+						commandName = cmd.Name
+					} else {
+						t.Error("called the wrong command's handler function")
+					}
+				})
+				test.Command.AddCommand(cmd)
+			}
+
+			cmd, err := test.Command.Parse(test.Args)
+			if err != nil {
+				t.Fatal(err)
+			}
+			cmd.Run()
+
+			if commandName == "" {
+				t.Error("no any command's handler function is called")
+			}
+		})
+	}
+}
+
+func TestFindHelp(t *testing.T) {
+	tests := []FindHelpTest{
+		{
+			Name:            "a simple help test",
+			Args:            []string{"help", "do"},
+			Command:         NewCommand("seo", "seo [command]", "seo command"),
+			ExpectedStrings: []string{"message value"},
+			SubCommands: []*Command{
+				{
+					Name: "do",
+					Use:  "seo do [flags]",
+					Parameters: []Parameter{
+						{
+							Name:        "message",
+							Shortname:   "m",
+							Description: "message value",
+						},
+					},
+					Flagset: flag.NewFlagSet("cli", flag.ContinueOnError),
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			for _, cmd := range test.SubCommands {
+				test.Command.AddCommand(cmd)
+			}
+
+			help, err := test.Command.FindHelp(test.Args)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			all := help.Usage + help.Parameters + help.Commands
+			for _, expectedString := range test.ExpectedStrings {
+				if !strings.Contains(all, expectedString) {
+					t.Errorf("help string is not contains any expected string")
+				}
+			}
+		})
+	}
+}
+
+func TestGetCurrentCommand(t *testing.T) {
+	tests := []GetCurrentCommandTest{
+		{
+			Name:                   "one simple command",
+			Args:                   []string{"do"},
+			Command:                NewCommand("seo", "seo [command]", ""),
+			SubCommands:            []*Command{NewCommand("do", "seo do [command]", "")},
+			ExpectedCurrentCommand: "do",
+		},
+		{
+			Name:    "a subcommand",
+			Args:    []string{"do", "concurrent"},
+			Command: NewCommand("seo", "seo [command]", ""),
+			SubCommands: []*Command{
+				{
+					Name: "do",
+					Use:  "seo do [command]",
+					Commands: []*Command{
+						{
+							Name: "concurrent",
+							Use:  "seo do concurrent",
+						},
+					},
+				},
+			},
+			ExpectedCurrentCommand: "concurrent",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			for _, cmd := range test.SubCommands {
+				test.Command.AddCommand(cmd)
+			}
+
+			command, _, err := test.Command.getCurrentCommand(test.Args)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if command.Name != test.ExpectedCurrentCommand {
+				t.Errorf("command name %s is not equal to the expected command name %s", command.Name, test.ExpectedCurrentCommand)
+			}
+		})
+	}
 }
 
 func TestParameters(t *testing.T) {
